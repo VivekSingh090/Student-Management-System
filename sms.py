f5ae576d653c9d18b6a1e36284abcb908fb1b185
Vivek Singh <singh.vivek9032@gmail.com>
2026-06-20 02:08:32 +0530

    Add view students feature

--- sms.py
+++ sms.py
@@ -31,4 +31,26 @@ def add_student():
 
 # Run
 add_student()
+# View all students
+
+
+def view_students():
+    cursor.execute("SELECT * FROM students")
+    students = cursor.fetchall()
+
+    if len(students) == 0:
+        print("No students found!")
+    else:
+        print("\n--- Student List ---")
+        print(f"{'ID':<5} {'Name':<20} {'Age':<5} {'Grade':<5}")
+        print("-" * 35)
+        for student in students:
+            print(
+                f"{student[0]:<5} {student[1]:<20} {student[2]:<5} {student[3]:<5}")
+        print("-" * 35)
+
+
+# Run
+add_student()
+view_students()
 conn.close()
